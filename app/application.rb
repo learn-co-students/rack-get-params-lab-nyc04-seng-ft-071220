dab13175b3840f6b57dc8e66ca08cea3933f6ca4
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-21 17:10:37 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+###################################################
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,15 +11,30 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0 
+        resp.write "Your cart is empty"
+      else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    end
+
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      resp.write check_item_in_cart(search_term)
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
+###################################################
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -27,4 +43,15 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def check_item_in_cart(item_in_cart)
+    if @@items.include?(item_in_cart)
+      @@cart << item_in_cart
+      return "added #{item_in_cart}"
+    else
+      return "We don't have that item"
+    end
+  end
+###################################################
+
 end
